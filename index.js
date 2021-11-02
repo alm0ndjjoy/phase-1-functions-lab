18be4351d59089c517f9b8a8c07257f3da7f8f8c
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-11-01 20:12:32 -0400

    commit tree 94d575a4d20cf2caefa46f50c759e2a97cc052e4

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solution in this file!
+// function distanceFromHqInBlocks(blocks){
+//     return Math.abs(blocks - 42);
+// }
+// distanceFromHqInBlocks(blocks);
+
+// function distanceFromHqInFeet(blocks){
+//     return distanceFromHqInBlocks(blocks)* 264;
+// }
+
+// function distanceTravelledInFeet(end,start){
+//     return Math.abs((end - start)* 264);
+// }
+
+// function calculatesFarePrice(end, start){
+    
+//     let distance = distanceTravelledInFeet(end, start);
+//     if (distance < 400){
+//         return 0;
+//     } else if (distance < 2000){
+//         return (distance - 400) * 0.02
+//     } else if (distance > 2000 && distance < 2500){
+//         return 25;
+//     } else{
+//         return "cannot travel that far"
+// }
+// }
+
+
+
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet)*264
+}
+function distanceTravelledInFeet(end, start){
+    return Math.abs((end - start) * 264)
+}
+
+function calculatesFarePrice(end, start){
+    let distance = distanceTravelledInFeet(end, start)
+    if(distance > 2500){
+        return "cannot travel that far"
+    } else if (distance > 2000){
+        return 25;
+    } else if (distance > 400){
+        return (distance - 400 )* 0.02
+    } else {
+        return 0
+    }
+
+}
